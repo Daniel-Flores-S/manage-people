e7ed3e35bb6fedff9281097e97fbaace94a9870e
Daniel <danielfloresda802@gmail.com>
2021-04-18 22:51:05 -0300

    new commit

--- src/main/java/one/courseSpring/personapi/service/PersonService.java
+++ src/main/java/one/courseSpring/personapi/service/PersonService.java
@@ -1,7 +1,9 @@
 package one.courseSpring.personapi.service;
 
+import one.courseSpring.personapi.dto.request.PersonDTO;
 import one.courseSpring.personapi.dto.response.MessageResponseDTO;
 import one.courseSpring.personapi.entity.Person;
+import one.courseSpring.personapi.mapper.PersonMapper;
 import one.courseSpring.personapi.repository.PersonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -9,6 +11,7 @@ import org.springframework.stereotype.Service;
 @Service
 public class PersonService {
     private PersonRepository personRepository;
+    private final PersonMapper personMapper = PersonMapper.INSTANCE;
 
     @Autowired
     public PersonService(PersonRepository personRepository) {
@@ -16,8 +19,9 @@ public class PersonService {
     }
 
 
-    public MessageResponseDTO createPerson(Person person) {
-        Person savedPerson = personRepository.save(person);
+    public MessageResponseDTO createPerson(PersonDTO personDTO) {
+        Person personToSave = personMapper.toModel(personDTO);
+        Person savedPerson = personRepository.save(personToSave);
         return  MessageResponseDTO
                 .builder()
                 .message("Created person with ID: "+ savedPerson.getId())
